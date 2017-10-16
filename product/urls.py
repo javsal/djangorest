0f7f49c4d460b7c3a7eae83c156f93187182e934
shamim4063 <shamim.vc@gmail.com>
2017-10-16 21:44:47 +0600

    function based views for product app

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from . import views
+from .views import product_list, product_detail
 
 urlpatterns = {
-    url(r'^list/$', views.product_list)
+    url(r'^list/$', product_list),
+    url(r'^list/(?P<pk>[0-9]+)/$', product_detail),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- product/views.py
+++ product/views.py
@@ -1,33 +1,48 @@
 from rest_framework import status
-from django.http import HttpResponse
-from rest_framework.renderers import JSONRenderer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
-import logging
-
-logger = logging.getLogger(__name__)
 
 
-class JSONResponse(HttpResponse):
+@api_view(['GET', 'POST'])
+def product_list(request):
     """
-    An HttpResponse that renders its content into JSON.
+    List all code snippets, or create a new snippet.
     """
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
 
-    
-    def product_list(request):
-        logger.error(request.method)
-        if request.method == 'GET':
-            products = Product.objects.all()
-            serializer = ProductSerializer(products, many=True)
-            return JSONResponse(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        if request.method=='POST':
-            serializer = ProductSerializer(data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return JSONResponse(serializer.data, status=status.HTTP_201_CREATED)
-            return JSONResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        product = ProductSerializer(product)
+        return Response(product.data)
+
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=product.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
